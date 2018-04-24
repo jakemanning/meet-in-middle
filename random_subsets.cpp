54c5c5d6d7d3b9c5f0ed5da1b78a4f0f2ad11fec
Jake Manning <jakemanning@ou.edu>
2018-04-24 03:46:48 -0500

    Improvements made to random subsets

--- random_subsets.cpp
+++ random_subsets.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <fstream>
@@ -10,12 +12,12 @@ using json = nlohmann::json;
 using namespace NTL;
 using namespace std;
 
-ZZ* fillArray(int n) {
+ZZ *fillArray(int n) {
     PrimeSeq seq;
-    auto * array = new ZZ[n];
+    auto *array = new ZZ[n];
     for (int i = 0; i < n; ++i) {
         RR primeNo(seq.next());
-        RR multiplier(1.0/3.0);
+        RR multiplier(1.0 / 3.0);
         RR res(floor(pow(primeNo, multiplier) * pow(to_RR(10), to_RR(100))));
         array[i] = to_ZZ(res);
     }
@@ -37,7 +39,7 @@ int main() {
 
     try {
         in >> input;
-    } catch(json::parse_error &error) {
+    } catch (json::parse_error &error) {
         cerr << error.what() << endl;
     }
 
@@ -46,14 +48,13 @@ int main() {
         double diff = input["logged_difference"];
         cout << "Previous best: " << diff << endl;
         min = threshold + to_ZZ(pow(to_RR(10), to_RR(diff)));
-    }
-    else {
+    } else {
         cout << "I was unable to find a previous result. Using default minimum" << endl;
         min = power(to_ZZ(10), 110);
     }
 
     do {
-        uniform_int_distribution<int> arraySizeDist(0, n - 1); // To get sample size
+        uniform_int_distribution<int> arraySizeDist(30, n - 1); // To get sample size (30 is the smallest possible sum with n=100)
         int sampleSize = arraySizeDist(mt);
         auto *sample = new int[sampleSize];
         uniform_int_distribution<int> sampleDistribution(0, sampleSize - 1);
@@ -69,26 +70,39 @@ int main() {
         }
 
         sum = to_ZZ(0);
+        int betterSampleSize = 1;
         for (int i = 0; i < sampleSize; ++i) {
             sum += array[sample[i]];
+            if (sum >= threshold) {
+                betterSampleSize = i + 1;
+                break;
+            }
         }
 
         if (sum >= threshold && sum < min) {
-            sort(sample, sample + sampleSize);
+            auto *betterSample = new int[betterSampleSize];
+            for (int j = 0; j < betterSampleSize; ++j) {
+                betterSample[j] = sample[j];
+            }
+            sort(betterSample, betterSample + betterSampleSize);
             min = sum;
             difference = sum - threshold;
-            cout << "Found a better one: " << log(difference)/log(10) << endl;
+            cout << "Found a better one: " << log(difference) / log(10) << endl;
 
             json j;
-            j["logged_difference"] = log(difference)/log(10);
-            for (int i = 0; i < sampleSize; ++i) {
-                j["array"].push_back(sample[i]);
+            auto now = chrono::system_clock::now();
+            time_t currentTime = chrono::system_clock::to_time_t(now);
+            j["time"] = ctime(&currentTime);
+            j["logged_difference"] = log(difference) / log(10);
+            for (int i = 0; i < betterSampleSize; ++i) {
+                j["array"].push_back(betterSample[i]);
             }
             ofstream out("lowest_difference.json");
             out << setw(2) << j << endl;
+            delete[] betterSample;
         }
         delete[] sample;
-    } while(true);
+    } while (true);
 
     return 0;
 }
