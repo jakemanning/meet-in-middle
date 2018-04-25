e7557fbf8423972b2cdef7e769ace040f1aad98e
Jake Manning <jakemanning@ou.edu>
2018-04-24 22:01:35 -0500

    Made more progress on actually meeting in middle

--- meet_in_middle.cpp
+++ meet_in_middle.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
+#include <set>
 #include "util.h"
 
 using namespace std;
 
+void outputArray(ZZ *array, int size) {
+    cout << "[ ";
+    for(int i = 0; i < size; ++i) {
+        cout << array[i];
+        if (i + 1 < size) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
 int main() {
     Util util(100, 113027942, "middle_lowest_difference.json");
+    int meetInMiddleSize = 50;
+    int quarterToIncludeSize = 25;
+    int bothSize = meetInMiddleSize + quarterToIncludeSize;
 
-    do {
-        auto *sample = util.takeRandomSample();
-        int subsetOfSample = util.sizeOfFirstSubsetOverThreshold(sample);
-        util.saveIfBetter(subsetOfSample, sample);
-        delete[] sample;
-    } while(true);
+    auto *removedQuarter = util.takeValueSample(bothSize, util.n, util.array); // Takes 75 values from the original 100 (discards 25 values)
+    auto *meetInMiddleIndices = util.takeIndexSample(meetInMiddleSize, bothSize); // Takes 50 indices from the 75 possible values
+    set<int> quarterSet; // The quarter that we will definitely include
+    for (int i = 0; i < bothSize; ++i) {
+        quarterSet.insert(i);
+    }
+    for (int i = 0; i < meetInMiddleSize; ++i) {
+        quarterSet.erase(meetInMiddleIndices[i]);
+    }
+    // quarterSet now contains 25 indices that we will use
+    auto quarterToInclude = new int[quarterToIncludeSize];
+    int index = 0;
+    for (auto it=quarterSet.begin(); it != quarterSet.end(); ++it, ++index) {
+        quarterToInclude[index] = *it;
+    }
+    delete[] quarterToInclude;
+
+    ZZ *quarter = util.convertToZZ(quarterToIncludeSize, removedQuarter, quarterToInclude);
+    ZZ *middle = util.convertToZZ(meetInMiddleSize, removedQuarter, meetInMiddleIndices);
+
+    outputArray(quarter, quarterToIncludeSize);
+    outputArray(middle, meetInMiddleSize);
+
+
+
     return 0;
 }
--- util.h
+++ util.h
@@ -16,25 +16,33 @@ using namespace std;
 
 class Util {
 private:
-    int n;
-    ZZ *array;
+
     ZZ threshold;
     string fileName;
     mt19937 mt{};
-    ZZ min;
+    ZZ currentMinimum;
     ZZ sum;
+    int minPossibleElementsInSubset = 0;
     int sampleSize = 0;
     ZZ *fillArray();
     ZZ readPreviousMin();
     void saveBest(int sampleSize, const int *betterSample, const ZZ &difference);
+    int smallestSubsetWithSum(ZZ arr[], int n, const ZZ &threshold);
 public:
+    int n;
+    ZZ *array;
     int *takeRandomSample(); // Random sample of indices
     json &getJson(ifstream &in, json &input) const;
     ZZ minValue(const json &input) const;
     int sizeOfFirstSubsetOverThreshold(const int *sample);
     void saveIfBetter(int size, const int *sample);
+    int *takeIndexSample(int sampleSize, int arraySize);
+    ZZ *takeValueSample(int sampleSize, int arraySize, ZZ *original);
 
     Util(int n, int id, string fileName);
+    ~Util();
+
+    ZZ *convertToZZ(int sampleSize, const ZZ *original, const int *indices) const;
 };
 
 Util::Util(int n, int id, string fileName) {
@@ -44,10 +52,10 @@ Util::Util(int n, int id, string fileName) {
     this->mt = mt;
     this->n = n;
     this->fileName = std::move(fileName);
-
     this->array = fillArray();
     this->threshold = to_ZZ(power(to_ZZ(10), 94) * 2 * id);
-    this->min = readPreviousMin();
+    this->currentMinimum = readPreviousMin();
+    this->minPossibleElementsInSubset = smallestSubsetWithSum(this->array, n, this->threshold);
 }
 
 ZZ *Util::fillArray() {
@@ -92,23 +100,42 @@ ZZ Util::minValue(const json &input) const {
 }
 
 int *Util::takeRandomSample() {
-    uniform_int_distribution<int> sizeDist(30, n - 1); // To get sample size (30 is the smallest possible sum with n=100)
+    uniform_int_distribution<int> sizeDist(minPossibleElementsInSubset, n - 1); // To get sample size (30 is the smallest possible sum with n=100)
     int sampleSize = sizeDist(mt);
-    auto *sample = new int[sampleSize]; // An array filled with indices of the original array
+    this->sampleSize = sampleSize;
+    return takeIndexSample(sampleSize, n);
+}
+
+int *Util::takeIndexSample(int sampleSize, int arraySize) {
+    auto *array = new int[sampleSize]; // An array filled with indices of the original array
     uniform_int_distribution<int> sampleDistribution(0, sampleSize - 1);
     uniform_real_distribution<double> realDistribution(0.0, 1.0); // Number btwn [0.0, 1.0)
 
     // Uses reservoir sampling to take a random sample consisting of 'sampleSize' from the Prime array
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < arraySize; ++i) {
         if (i < sampleSize) {
-            sample[i] = i;
+            array[i] = i;
         } else if (i >= sampleSize && (realDistribution(mt) < sampleSize / ((float) (i + 1)))) {
             int replace = sampleDistribution(mt);
-            sample[replace] = i;
+            array[replace] = i;
         }
     }
-    this->sampleSize = sampleSize;
-    return sample;
+    return array;
+}
+
+ZZ *Util::takeValueSample(int sampleSize, int arraySize, ZZ *original) {
+    int *indices = takeIndexSample(sampleSize, arraySize);
+    ZZ *ret = convertToZZ(sampleSize, original, indices);
+    delete[] indices;
+    return ret;
+}
+
+ZZ *Util::convertToZZ(int sampleSize, const ZZ *original, const int *indices) const {
+    auto *newValues = new ZZ[sampleSize];
+    for (int i = 0; i < sampleSize; ++i) {
+        newValues[i] = original[indices[i]];
+    }
+    return newValues;
 }
 
 int Util::sizeOfFirstSubsetOverThreshold(const int *sample) {
@@ -126,13 +153,13 @@ int Util::sizeOfFirstSubsetOverThreshold(const int *sample) {
 }
 
 void Util::saveIfBetter(int size, const int* sample) {
-    if (sum >= threshold && sum < min) {
+    if (sum >= threshold && sum < currentMinimum) {
         auto *betterSample = new int[size];
         for (int j = 0; j < size; ++j) {
             betterSample[j] = sample[j];
         }
         sort(betterSample, betterSample + size);
-        min = sum;
+        currentMinimum = sum;
         ZZ difference(sum - threshold);
         cout << "Found a better one: " << log(difference) / log(10) << endl;
         saveBest(size, betterSample, difference);
@@ -151,6 +178,41 @@ void Util::saveBest(int sampleSize, const int *betterSample, const ZZ &differenc
         }
     ofstream out(this->fileName);
     out << setw(2) << j << endl;
+}
+
+// Returns length of smallest subarray with sum greater than threshold.
+// If there is no subarray with given sum, then returns n+1
+int Util::smallestSubsetWithSum(ZZ arr[], int n, const ZZ &threshold) {
+    // Initialize current sum and minimum length
+    ZZ currSum(0);
+    int min_len = n+1;
+
+    // Initialize starting and ending indexes
+    int start = 0, end = 0;
+    while (end < n)
+    {
+        // Keep adding array elements while current sum
+        // is smaller than threshold
+        while (currSum <= threshold && end < n) {
+            currSum += arr[end++];
+        }
+
+        // If current sum becomes greater than threshold.
+        while (currSum > threshold && start < n) {
+            // Update minimum length if needed
+            if (end - start < min_len)
+                min_len = end - start;
+
+            // remove starting elements
+            currSum -= arr[start++];
+        }
+    }
+    return min_len;
+}
+
+Util::~Util() {
+    delete[] array;
+    array = nullptr;
 }
 
 #endif //RANDOM_SHARED_H
