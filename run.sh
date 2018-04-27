5086cfd9464fce24fd7118b722bfe66c6fc7a4d1
Jake Manning <jakemanning@ou.edu>
2018-04-27 19:30:49 +0000

    Store output folders in a directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 /home/linuxbrew/.linuxbrew/Cellar/cmake/3.11.1/bin/cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" /home/jakemanning/meet-in-middle/
 /home/linuxbrew/.linuxbrew/Cellar/cmake/3.11.1/bin/cmake --build ./ --target middle -- -j 4
-(./middle "$@" &> ~/$$-output.log &)
+(./middle "$@" &> ~/output/$$-output.log &)
 echo "Running"
-tail -f ~/$$-output.log | less
+tail -f ~/output/$$-output.log | less
 
