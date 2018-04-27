ed7af53f7bf09b60860f07d57dc73c6763393e88
Jake Manning <jakemanning@ou.edu>
2018-04-27 19:27:14 +0000

    Ensure output won't overlap by using pid

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 /home/linuxbrew/.linuxbrew/Cellar/cmake/3.11.1/bin/cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" /home/jakemanning/meet-in-middle/
 /home/linuxbrew/.linuxbrew/Cellar/cmake/3.11.1/bin/cmake --build ./ --target middle -- -j 4
-nohup./middle "$@" &> output.txt &
+rm ~/output.log
+(./middle "$@" &> ~/$$-output.log &)
 echo "Running"
-tail -f output.txt | less
+tail -f ~/output.log | less
